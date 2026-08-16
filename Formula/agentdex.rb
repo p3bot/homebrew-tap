ece50906ea441b2987f5e51c68240946d2d6bf21
Grant Carthew <grant@carthew.net>
2026-08-16 18:49:49 +1000

    agentdex: bump to v2.0.0

--- Formula/agentdex.rb
+++ Formula/agentdex.rb
@@ -1,8 +1,8 @@
 class Agentdex < Formula
   desc "Detect AI coding agents installed on the local machine"
   homepage "https://github.com/p3bot/agentdex"
-  url "https://github.com/p3bot/agentdex/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "99520aa789bfdd3c147ef12d7002d3893ff2caf1c789665101e079d5126db208"
+  url "https://github.com/p3bot/agentdex/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "1ef62982e98f651e2e96578e549e75235ec61a55a430097612a918ae63e84f45"
   license "MPL-2.0"
 
   depends_on "go" => :build
@@ -12,7 +12,7 @@ class Agentdex < Formula
     ldflags = %W[
       -s -w
       -X github.com/p3bot/agentdex/internal/cli.Version=#{version}
-      -X github.com/p3bot/agentdex/internal/cli.Commit=62b68205df4f9f69f1f2c6e8d23f9b52d690d4cf
+      -X github.com/p3bot/agentdex/internal/cli.Commit=f5b3e3d2adc5df0cd8c5658ddc9ec8fa4c1958b5
       -X github.com/p3bot/agentdex/internal/cli.Date=#{time.iso8601}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/agentdex"
